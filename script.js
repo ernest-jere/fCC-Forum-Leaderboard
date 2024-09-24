6637ab570606f43f844d7d525ff62a099c9df0d0
Ernest Ernest <130362988+ernest-jere@users.noreply.github.com>
2024-09-24 15:43:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,133 @@
-let price = 19.5;
-let cid = 
-  [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]
-; 
-let cash = document.getElementById("cash");
-const purchaseBtn = document.getElementById("purchase-btn");
-const changeDue = document.getElementById("change-due")
+const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest.json";
+const forumTopicUrl = "https://forum.freecodecamp.org/t/";
+const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
+const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 
-purchaseBtn.addEventListener("click", () => {
-  if (Number(cash.value) < price) {
-    alert("Customer does not have enough money to purchase the item");
+const postsContainer = document.getElementById("posts-container");
+
+const allCategories = {
+  299: { category: "Career Advice", className: "career" },
+  409: { category: "Project Feedback", className: "feedback" },
+  417: { category: "freeCodeCamp Support", className: "support" },
+  421: { category: "JavaScript", className: "javascript" },
+  423: { category: "HTML - CSS", className: "html-css" },
+  424: { category: "Python", className: "python" },
+  432: { category: "You Can Do This!", className: "motivation" },
+  560: { category: "Backend Development", className: "backend" },
+};
+
+const forumCategory = (id) => {
+  let selectedCategory = {};
+
+  if (allCategories.hasOwnProperty(id)) {
+    const { className, category } = allCategories[id];
+
+    selectedCategory.className = className;
+    selectedCategory.category = category;
+  } else {
+    selectedCategory.className = "general";
+    selectedCategory.category = "General";
+    selectedCategory.id = 1;
   }
-  if (Number(cash.value) === price) {
-    changeDue.textContent = "No change due - customer paid with exact cash";
-  }
-  if (Number(cash.value) > price) {
-    changeDue.textContent = CheckTransactionStatus(price, cash, cid);
-  } 
-})
+  const url = `${forumCategoryUrl}${selectedCategory.className}/${id}`;
+  const linkText = selectedCategory.category;
+  const linkClass = `category ${selectedCategory.className}`;
 
-function CheckTransactionStatus(price, cash, cid) {
-  const currencyUnit = [
-    ["PENNY", 0.01],
-    ["NICKEL", 0.05],
-    ["DIME", 0.1],
-    ["QUARTER", 0.25],
-    ["ONE", 1.0],
-    ["FIVE", 5.0],
-    ["TEN", 10.0],
-    ["TWENTY", 20.0],
-    ["ONE HUNDRED", 100.0]
-  ];
+  return `<a href="${url}" class="${linkClass}" target="_blank">
+    ${linkText}
+  </a>`;
+};
 
-  let change = Number(cash.value) - price;
-  let changeArray = [];
+const timeAgo = (time) => {
+  const currentTime = new Date();
+  const lastPost = new Date(time);
 
-  for (let i = currencyUnit.length - 1; i >= 0; i--) {
-    let coinName = currencyUnit[i][0];
-    let coinValue = currencyUnit[i][1];
-    let coinAmount = 0;
+  const timeDifference = currentTime - lastPost;
+  const msPerMinute = 1000 * 60;
 
-    while (change >= coinValue && cid[i][1] > 0) {
-      change -= coinValue;
-      cid[i][1] -= coinValue;
-      coinAmount += coinValue;
-      change = Math.round(change * 100) / 100; 
-    }
+  const minutesAgo = Math.floor(timeDifference / msPerMinute);
+  const hoursAgo = Math.floor(minutesAgo / 60);
+  const daysAgo = Math.floor(hoursAgo / 24);
 
-    if (coinAmount > 0) {
-      changeArray.push([coinName + ":", "$" + parseFloat(coinAmount).toFixed(2) + " "].join(" "));
-    }
-  }
-
-  if (change > 0) {
-    return  `status: INSUFFICIENT_FUNDS ${[]}` ;
+  if (minutesAgo < 60) {
+    return `${minutesAgo}m ago`;
   }
 
-  if (cid.reduce((total, item) => total + item[1] === change)) {
-    return  `status: CLOSED ${changeArray}` ;
+  if (hoursAgo < 24) {
+    return `${hoursAgo}h ago`;
   }
 
-  return  `Status: OPEN ${changeArray.join(" ")}` ;
-}
+  return `${daysAgo}d ago`;
+};
+
+const viewCount = (views) => {
+  const thousands = Math.floor(views / 1000);
+
+  if (views >= 1000) {
+    return `${thousands}k`;
+  }
+
+  return views;
+};
+
+const avatars = (posters, users) => {
+  return posters
+    .map((poster) => {
+      const user = users.find((user) => user.id === poster.user_id);
+      if (user) {
+        const avatar = user.avatar_template.replace(/{size}/, 30);
+        const userAvatarUrl = avatar.startsWith("/user_avatar/")
+          ? avatarUrl.concat(avatar)
+          : avatar;
+        return `<img src="${userAvatarUrl}" alt="${user.name}" />`;
+      }
+    })
+    .join("");
+};
+
+const fetchData = async () => {
+  try {
+    const res = await fetch(forumLatest);
+    const data = await res.json();
+    showLatestPosts(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchData();
+
+const showLatestPosts = (data) => {
+  const { topic_list, users } = data;
+  const { topics } = topic_list;
+
+  postsContainer.innerHTML = topics.map((item) => {
+    const {
+      id,
+      title,
+      views,
+      posts_count,
+      slug,
+      posters,
+      category_id,
+      bumped_at,
+    } = item;
+
+    return `
+    <tr>
+      <td>
+        <a class="post-title" target="_blank" href="${forumTopicUrl}${slug}/${id}">${title}</a>
+
+        ${forumCategory(category_id)}
+      </td>
+      <td>
+        <div class="avatar-container">
+          ${avatars(posters, users)}
+        </div>
+      </td>
+      <td>${posts_count - 1}</td>
+      <td>${viewCount(views)}</td>
+      <td>${timeAgo(bumped_at)}</td>
+    </tr>`;
+  }).join("");
+};
